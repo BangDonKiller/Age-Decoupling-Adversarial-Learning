fix(GSP): fix the mean and std calculation in the GSP module - model.py: 調整speaker_embedding_layer的神經層 - GSP.py: 修正全局統計池化層的均值和標準差計算

model.py
@@ -56,11 +56,9 @@ class ADAL_Model(nn.Module):
         # 全局統計池化層 + 說話者嵌入層 => 提取出 z
         self.speaker_embedding_layer = nn.Sequential(
             GlobalStatisticalPooling(),
-            nn.Linear(512 * 2 * 3, 1024),
+            nn.Linear(512 * 2, feature_dim),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(1024, feature_dim),
-            nn.ReLU(inplace=True)
+            nn.Dropout(0.5)
         )
         
         # 年齡特徵提取模塊 (ARE)

modules/GSP.py
@@ -7,18 +7,12 @@ class GlobalStatisticalPooling(nn.Module):
 
     def forward(self, x):
         # x 的形狀是 (batch_size, channels, height, width_frames)
-        # 例如：(B, 512, 3, W_final)
         # 1. 沿著時間維度 (dim=-1) 計算均值和標準差
-        # 輸出形狀： (batch_size, channels, height)
-        mean = x.mean(dim=-1)
-        std = x.std(dim=-1)
-
-        # 2. 將均值和標準差在新的維度上拼接起來
-        # 輸出形狀： (batch_size, 2 * channels, height)
-        pooled_features = torch.cat((mean, std), dim=1)
+        mean = x.mean(dim=[-2, -1], keepdim=False)
+        std = x.std(dim=[-2, -1], keepdim=False) + 1e-6  # 防止除以零
         
-        # 3. 將這些pooled features展平為一個向量，送入全連接層
-        # 輸出形狀： (batch_size, 2 * channels * height)
-        z = pooled_features.view(x.size(0), -1) 
+        # 2. 將均值和標準差連接起來
+        # z 的形狀是 (batch_size, channels * 2)
+        z = torch.cat((mean, std), dim=1)
         
         return z